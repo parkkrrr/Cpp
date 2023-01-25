e21ecfbd7ce84c13ab5b1172b5790cf759cf9b22
parkkrrr <prakharverma2019@gmail.com>
2023-01-25 21:47:39 +0530

    new file:   helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //Preprocessor Directive - it gives instruction to the compiler to preprocess some code before actual compilation starts.
+using namespace std;
+
+int main() {        // execution begins from here
+    cout << "Hello World";
+    return 0;
+}
