942def674e625c9f27faa6a046f96e76d67784c7
parkkrrr <prakharverma2019@gmail.com>
2023-01-27 17:46:23 +0530

    new file:   sizeof.cpp

---
+++ sizeof.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <iostream>
+using namespace std;
+
+// Function prototype
+void dataTypes(int, float, double, long long, string);
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+// Function to find size of different data types
+void dataTypes(int a, float b, double c, long long l, string d){
+     a = b/c;
+     c = b/a;
+     b = c/a;
+     l = b+l;
+   
+     cout<<sizeof(a)<<" "<<sizeof(c)<<" "<<sizeof(b)<<" "<<sizeof(l)<<endl;
+     cout<<sizeof(d)<<" "<<sizeof(d[3])<<endl;
+  
+
+}
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{
+    int a;
+    float b;
+    double c;
+    long long l;
+    string d;
+    
+    int t;
+    cin >> t;
+    
+    while(t--){
+        
+        cin >> a >> b >> c >> l >> d;
+    
+        dataTypes(a, b, c, l, d);
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
