451916f05fc55877d5e0893ccb999a2b31e2bed5
parkkrrr <prakharverma2019@gmail.com>
2023-01-27 17:16:06 +0530

    new file:   macros.cpp

---
+++ macros.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using std::cin;
+using std::cout;
+
+#define multiply(a, b) (a * b)
+
+void macros(int a, int b)
+{
+    cout << multiply(a, b);
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    macros(a, b);
+
+    return 0;
+}
