58200122e81b7d77166333237520c29748700a48
Prakhar Verma <prakharverma2019@gmail.com>
2023-05-13 20:36:07 +0530

    new file:   DSA/.vscode/settings.json 	new file:   DSA/.vscode/tasks.json 	new file:   DSA/array/kadaneAlgorithm.cpp 	new file:   DSA/array/print_all_pairs.cpp 	new file:   DSA/array/print_subArray.cpp 	renamed:    DSA/rotate_array.cpp -> DSA/array/rotate_array.cpp 	new file:   DSA/array/subarraySum.cpp

---
+++ DSA/array/kadaneAlgorithm.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<INT_MIN;
+
+    return 0;
+}
---
+++ DSA/array/subarraySum.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int maxSumSubarray(int arr[], int n)
+{
+    int maxSum{};
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            int sumSubarray{};
+            for (int k = i; k <= j; k++)
+                sumSubarray += arr[k];
+            maxSum = max(maxSum, sumSubarray);
+        }
+    }
+    return maxSum;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 5, 2, 3, 4, -1, -5, 1, -5, 6, 7};
+    int n{sizeof(arr) / sizeof(int)};
+
+    cout << maxSumSubarray(arr, n);
+    return 0;
+}
