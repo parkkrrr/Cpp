c3cd37c356adb3b24e62b26705855ca64ed4cce8
Prakhar Verma <prakharverma2019@gmail.com>
2023-08-20 15:14:28 +0530

    modified:   DSA/pyramid.cpp

--- DSA/pyramid.cpp
+++ DSA/pyramid.cpp
@@ -7,13 +7,13 @@ int main(){
     cin>>n;
 
     for(int i=1; i<n; i++){
-        for(int j=1; j<n-1; j++){
+        for(int j=1; j<n-i;j++)
             cout<<" ";
-        }
-        cout<<endl;
-        for(int k=i; k<2*i-1; k++){
+        for(int k=1; k<=(2*i-1);k++)
             cout<<i;
-        }
+        
+        cout<<endl;
+
     }
     return 0;
 }
