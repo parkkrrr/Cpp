bde5697397bb8b8542921e8e3a951268fa6f5a1f
Prakhar Verma <prakharverma2019@gmail.com>
2023-08-20 15:20:55 +0530

    modified:   DSA/pyramid.cpp

--- DSA/pyramid.cpp
+++ DSA/pyramid.cpp
@@ -5,13 +5,15 @@ int main(){
     int n;
     cout<<"Enter a number : ";
     cin>>n;
-
+    int count=0;
     for(int i=1; i<n; i++){
         for(int j=1; j<n-i;j++)
             cout<<" ";
-        for(int k=1; k<=(2*i-1);k++)
-            cout<<i;
-        
+        for(int k=1; k<=(2*i-1);k++){
+            cout<<count;
+            count++;
+        }
+        count=1;
         cout<<endl;
 
     }
