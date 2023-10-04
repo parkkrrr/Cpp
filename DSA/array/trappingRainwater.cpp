f14e952c1c6de910759415f19b0ecf1a4d8e652e
Prakhar Verma <prakharverma2019@gmail.com>
2023-10-05 01:50:57 +0530

    modified:   DSA/array/trappingRainwater.cpp

--- DSA/array/trappingRainwater.cpp
+++ DSA/array/trappingRainwater.cpp
@@ -6,8 +6,22 @@ void calculatingRainwater(int height[], int n)
     if (n < 2)
         return;
     int trapped {};
-    int minBoundary {*min_element(height,height+n)};
-    int maxBoundary {*max_element(height,height+n)};
+    int waterLevel{};
+    // int minBoundary {*min_element(height,height+n)};
+    // int maxBoundary {*max_element(height,height+n)};
+    int leftMaxBoundary[n];
+    int rightMaxBoundary[n];
+    leftMaxBoundary[0]=height[0];
+    rightMaxBoundary[n-1]=height[n-1];
+
+    for(int i=1;i<n;i++)
+        leftMaxBoundary[i]=max(height[i],leftMaxBoundary[i-1]);
+    
+    for(int i=n-2;i>=0;i--)
+        rightMaxBoundary[i]=max(height[i],rightMaxBoundary[i+1]);
+        
+    
+    
     // while(n--){
     //     if(height[n]>maxBoundary)
     //         maxBoundary=height[n];
@@ -18,20 +32,23 @@ void calculatingRainwater(int height[], int n)
     // }
 
     // int sum {};
-    for (int i = 1; i < n - 1; i++)
+    for (int i = 0; i < n ; i++)
     {
         // if ((height[i - 1] > height[i] && height[i + 1]) > height[i])
         // {
         //     int mini = min(height[i - 1], height[i + 1]);
-            trapped += mini - height[i];
-        }
+            // trapped += mini - height[i];
+        // }
+        waterLevel=min(leftMaxBoundary[i],rightMaxBoundary[i]);
+        trapped+=waterLevel-height[i];
     }
     cout<< trapped<<endl;
 }
 
 int main()
 {
-    int height[]{4,2,0,6,3,2,5};
+    // int height[]{4,2,0,6,3,2,5};
+    int height[]{7,4,0,9};
     int n = sizeof(height) / sizeof(height[0]);
 
     calculatingRainwater(height, n);
